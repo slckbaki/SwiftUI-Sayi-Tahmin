14912c20cbfa1402d780f263eb98eca73f84e1da
selcuk baki <selcukbaki@windowslive.com>
2021-09-20 00:09:11 +0300

    Initial Commit

---
+++ SayiTahminOyunu/SayiTahminOyunuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayiTahminOyunuApp.swift
+//  SayiTahminOyunu
+//
+//  Created by Selcuk Baki on 20/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SayiTahminOyunuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
